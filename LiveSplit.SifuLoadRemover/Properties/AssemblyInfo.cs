c172f7d861735c483b2ddaaacc75691f457bccf9
José Marciel Bergamo Junior <jmbergamoj@gmail.com>
2022-02-14 15:58:30 -0300

    Updated Binaries to work with new Update URL
    Removed dummy Update from updates file

--- LiveSplit.SifuLoadRemover/Properties/AssemblyInfo.cs
+++ LiveSplit.SifuLoadRemover/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LiveSplit.SifuLoadRemover")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Load Remover tool for LiveSplit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LiveSplit.SifuLoadRemover")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-US")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
